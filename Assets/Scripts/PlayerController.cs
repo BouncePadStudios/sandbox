dad726b27b6790b4d4a51bb2855807fd7280c1e6
Eren Sulutas <erensulu11@gmail.com>
2020-05-05 17:37:38 -0400

    typo
    oop

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController2D : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 
     private Animator animator;
